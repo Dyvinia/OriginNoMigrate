f8b122160cca44b17b17097aa7ff1feb1c39d625
Dyvinia <dulana57@gmail.com>
2023-03-14 20:33:22 -0400

    Downgrade Origin & prevent it from updating itself

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
+using Microsoft.Win32;
 
 // die :3
 Console.WriteLine("Killing Origin and EA Desktop");
@@ -10,6 +12,56 @@ foreach (Process process in Process.GetProcessesByName("Origin")) process.Kill()
 
 string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), @"Origin\local.xml");
 string newSetting = "  <Setting value=\"true\" key=\"MigrationDisabled\" type=\"1\"/>";
+string originPath = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\Origin")?.GetValue("OriginPath")?.ToString();
+
+
+// Version check Origin
+try {
+    Version originVersion = new(FileVersionInfo.GetVersionInfo(originPath).FileVersion.Replace(",", "."));
+    Version finalVersion = new("10.5.119.0");
+
+    // Origin is too recent
+    if (originVersion.CompareTo(finalVersion) >= 0) {
+        Console.WriteLine("You must install an older version of Origin");
+        Console.WriteLine("Origin v10.5.118.52644: https://cdn.discordapp.com/attachments/1054968664807452812/1085342027383832627/OriginSetup.exe");
+
+        Console.Write("Press Enter to Close This Window...");
+        Console.ReadLine();
+        Environment.Exit(0);
+    }
+}
+catch (Exception ex) {
+    Console.WriteLine(ex.ToString());
+}
+
+
+try {
+    string originSetupInternal = originPath.Replace("Origin.exe", "OriginSetupInternal.exe");
+    string originThinSetupInternal = originPath.Replace("Origin.exe", "OriginThinSetupInternal.exe");
+
+    if (File.Exists(originSetupInternal)) {
+        Console.WriteLine($"Deleting {originSetupInternal}");
+        File.Delete(originSetupInternal);
+    }
+
+    if (File.Exists(originThinSetupInternal)) {
+        Console.WriteLine($"Deleting {originThinSetupInternal}");
+        File.Delete(originThinSetupInternal);
+    }
+}
+catch {
+    Console.WriteLine("Missing Permissions to delete file, Restarting as Administrator...");
+    Console.Write("Press Any Key to Continue...");
+    Console.ReadKey();
+
+    // No need for arguments since it can only reach this code if there are no arguments
+    Process.Start(new ProcessStartInfo {
+        FileName = Environment.ProcessPath,
+        UseShellExecute = true,
+        Verb = "runas"
+    });
+    Environment.Exit(0);
+}
 
 try {
     Console.WriteLine($"Opening {path}");
